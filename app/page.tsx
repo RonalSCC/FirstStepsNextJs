c59371e0b4280b37e91ff9ac6cdca1642c8a4fda
Ronal Castaño <ronalch99@outlook.com>
2024-05-18 18:58:37 -0500

    Publish

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-7xl">Hola mundo</h1>
+      <button className="bg-blue-700 py-3 px-2 border-4" >
+        <Link href={"./about"}>
+          Ir a funcionalidad
+        </Link>
+      </button>
     </main>
   );
 }
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { ActiveLink } from '../activeLink/ActiveLink';
 
 const itemsMenu = [
@@ -8,7 +9,7 @@ const itemsMenu = [
 export const Navbar = () => {
   return (
     <nav className="flex gap-2 bg-blue-400 bg-opacity-30 p-2 m-2 rounded">
-        <span>Home</span>
+        <Link href={"./"}>Home</Link>
 
         <div className="flex flex-1"></div>
         {
